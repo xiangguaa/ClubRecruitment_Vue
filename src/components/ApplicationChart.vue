2e09fea18e63c44a2625d25e346a85e83dbc9884
shuo.zheng <428384851@qq.com>
2020-04-09 14:02:00 +0800

    基本完成表单组件

--- src/components/ApplicationChart.vue
+++ src/components/ApplicationChart.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>
-      application chart |
-      {{clubName}}
+  <div class="mask">
+      <div class="wrp">
+          <p class="title">{{this.chart.header.value}}</p>
+          <form action="" method="post">
+            <div class="body">
+                <div :style="[item.infoStyle.entireRow==true?itemStyle2:itemStyle]" 
+                    class="main-item"  
+                    v-for="(item,idx) in chart.body" :key="idx">
+                        <div v-if="item.isCheckbox==false" 
+                            class="input-item">
+                            <label class="item-name">{{item.infoValue}}</label>
+                            <input  class="item-input"
+                                :type="[item.isCheckbox==true?'':'text']"
+                                :style="[item.infoStyle.entireRow==true?styleObject2:styleObject]"
+                                name="name" id="name" required>     
+                        </div>
+
+                        <div v-else class="checkbox-item">
+                            <label class="item-name">{{item.infoName}}</label>
+                            <div class="checbox-wrp">
+                                <div class="citem"
+                                    v-for="(checkitem,checkIdx) in item.infoValue"
+                                    :key="checkIdx">
+                                    <input class="checkbox-input"
+                                        
+                                        type="checkbox"
+                                        :style="[item.infoStyle.entireRow==true?styleObject2:styleObject]"
+                                        name="name" id="name" required>{{checkitem}}  
+                                </div>
+                               
+                            </div>
+                               
+                        </div>
+                             
+                        <!-- {{item.infoName}} - {{item.infoValue}} - {{item.infoStyle}} -->
+                </div>
+            </div>
+            <div class="notes">
+                <div class="note-item" v-for="(item,idx) in chart.footer" :key="idx">
+                    {{item}}
+                </div>
+            </div>
+          </form>
+      </div>
   </div>
 </template>
 
@@ -9,12 +50,124 @@
 export default {
     props: {
         clubName:String,
+        chart:Object,
     },
-    name:"ApplicationChart"
+    name:"ApplicationChart",
+    created:function(){
+        console.log(this.chart)
+    },
+    data:function(){
+        return {
+            itemStyle: {
+                color: 'red',
+                fontSize: '13px',
+                backgroudColor:'blue',
+                width:'49%',
+                border:'solid 2px black',
+                boxSizing: 'border-box',
+            },
+            itemStyle2: {
+                color: 'blue',
+                fontSize: '13px',
+                backgroudColor:'red',
+                width:'100%',
+                border:'solid 2px black',
+                boxSizing: 'border-box',
+            }
+        }
+    }
 
 }
 </script>
+    
 
 <style>
 
+    .wrp{
+        background-color: gray;
+        width: 80%;
+        /* height: 55rem; */
+        margin: 1rem auto;
+        border: black solid 0.5px;
+    }
+    .title{
+        width: 100%;
+        height: 2rem;
+        line-height: 2rem;
+        margin: 1rem 0rem 1rem 0rem;
+
+        padding: 1rem 0rem 1rem 0rem;
+        font-size: 2rem ;
+    }
+    form{
+
+    }
+    .body{
+
+        width: inherit;
+        height: 40rem;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+    .main-item{
+        background-color: khaki;
+        height: 5rem;
+        display: flex;
+        align-items: center;
+        
+    }
+    .input-item{
+        width: 100%;
+        background-color: lemonchiffon;
+        padding-left: 1rem;
+        display: flex;
+        justify-content: start;
+        align-items: center;
+    }
+    .item-name{
+        font-size: 1.5rem;
+        width: 20%;
+        margin: 0rem;
+        padding: 0;
+        display: flex;
+        justify-content: start;
+        /* background-color: magenta; */
+    }
+    .item-input{width: 80%;}
+
+    .checkbox-item{
+        padding-left: 1rem;
+        width: 100%;
+        background-color: lightcoral;
+        display: flex;
+        justify-content: start;
+        align-items: center;
+    }
+    
+    
+    .checbox-wrp{
+        width: 80%;
+        background-color: lightslategray;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+    .citem{
+        background-color: maroon;
+    }
+    
+    .checkbox-input{
+        background-color: lime;
+    }
+    .chebox-wrp{
+        width: 100%;
+        /* background-color: lawngreen; */
+        display: flex;
+
+    }
+
+    .notes{
+        background-color: rgb(151, 151, 151);
+    }
 </style>
--- src/views/Joinchart.vue
+++ src/views/Joinchart.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <timeline :timeline="schedule" :clubName="clubName"></timeline>
-        <ap-chart :clubName="clubName"></ap-chart>
+        <ap-chart :clubName="clubName" :chart="chart"></ap-chart>
         <!-- <router-link :to="{name:'t'}">所发的发射点法大师傅阿松大</router-link> -->
         <!-- <t2></t2> -->
         <!-- <router-view></router-view> -->
@@ -14,21 +14,19 @@ import t2 from "../views/test2.vue"
 export default {
     data: function () {
         return {
-            schedule: []
+            schedule: [],
+            chart:{},
         }
     },
-    // data:{
-    //     schedule:[]
-    // },
     name:'Joinchart',
     props: {
         clubName:String
     },
 
     beforeCreate: function () {
-        var aData = new Date();
-        console.log(aData) //Wed Aug 21 2019 10:00:58 GMT+0800 (中国标准时间)
-        console.log(this)
+        // var aData = new Date();
+        // console.log(aData) //Wed Aug 21 2019 10:00:58 GMT+0800 (中国标准时间)
+        // console.log(this)
     },
     created:function(){
         console.log(this.clubName)
@@ -42,7 +40,29 @@ export default {
                         ]
         // axios.get('https://a.b.com?clubName='+this.clubName)
         // 返回该社团的招新问卷表结构
-        this.chart = {}
+        this.chart = {
+            'header':{'value':"辩论队招新报名表"},
+            'body':[
+                {'infoName':'姓名','infoValue':'姓名','infoStyle':{'entireRow':false,'fontWeight':'bold'},'isCheckbox':false},
+                {'infoName':'性别','infoValue':'性别','infoStyle':{'entireRow':false,'fontWeight':'bold'},'isCheckbox':false},
+                {'infoName':'学号','infoValue':'学号','infoStyle':{'entireRow':false,'fontWeight':'bold'},'isCheckbox':false},
+                {'infoName':'电话','infoValue':'电话','infoStyle':{'entireRow':false,'fontWeight':'bold'},'isCheckbox':false},
+                {'infoName':'学院','infoValue':'学院','infoStyle':{'entireRow':false,'fontWeight':'bold'},'isCheckbox':false},
+                {'infoName':'QQ','infoValue':'QQ','infoStyle':{'entireRow':false,'fontWeight':'bold'},'isCheckbox':false},
+                {'infoName':'笔试时间','infoValue':['aa','bb','cc'],'infoStyle':{'entireRow':true,'fontWeight':'light'},'isCheckbox':true},
+                {'infoName':'奖项/比赛经历','infoValue':'奖项/比赛经历','infoStyle':{'entireRow':true,'fontWeight':'light'},'isCheckbox':false},
+                {'infoName':'自我介绍','infoValue':'自我介绍','infoStyle':{'entireRow':true,'fontWeight':'light'},'isCheckbox':false},
+                
+
+            ],
+            'footer':[
+                'dsfasdfasdf',
+                '大家发来减肥哈哈哈我和嘎哈大哥啊大哥拉回到发生的的立法和斯大林发送大量法律上的封建落后阿斯弗舒服多啦合适的方式发挥了',
+                '觉得拉法基类毒素解放拉萨地方啊深度发掘建立了上的飞机啊'
+            ]
+        }
+        console.log(this.chart)
+        console.log(this.chart.header)
     },
     components:{
         t2
